0c8b279d33a384dba48325341936124711fa2151
Matias Urrutia <270664754+Matyhb99@users.noreply.github.com>
2026-05-25 21:24:08 -0400

    creacion del codigo en navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,1 +1,10 @@
-
+export default function Navbar() {
+  return (
+    <div className="navbar">
+      <input
+        type="text"
+        placeholder="Buscar en el informe..."
+      />
+    </div>
+  );
+}
