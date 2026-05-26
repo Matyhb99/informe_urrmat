11afc585215e0682fec62791a58060245d6f7602
Matias Urrutia <270664754+Matyhb99@users.noreply.github.com>
2026-05-25 21:32:02 -0400

    creacion y modificacion del markdownpage.jsx

--- src/pages/MarkdownPage.jsx
+++ src/pages/MarkdownPage.jsx
@@ -1,1 +1,11 @@
+import ReactMarkdown from "react-markdown";
 
+export default function MarkdownPage({ content }) {
+  return (
+    <div className="markdown-container">
+      <ReactMarkdown>
+        {content}
+      </ReactMarkdown>
+    </div>
+  );
+}
